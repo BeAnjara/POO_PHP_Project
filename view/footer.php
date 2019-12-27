81456bc16f8f0f06955b448f22ac22d7ceb9e610
BeAnjara <beanjarahoussen@gmail.com>
2019-12-27 15:24:34 +0300

    Header and footer

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+</div>
+<script src="lib/js/jquery-3.3.1.min.js"></script>
+<script src="lib/js/popper.min.js"></script>
+<script src="lib/js/bootstrap.min.js"></script>
+<script src="lib/js/registration_validation.js"></script>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Benz Art Mada</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="lib/css/bootstrap.min.css">
+	<link rel="stylesheet" href="lib/css/registration.css">
+</head>
+<body>
+
+<div class="container">
